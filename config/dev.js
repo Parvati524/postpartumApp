e6a7aa9d17f62bd2611dbf6df0e0b72a2084e246
Parvati Mani <parumani@gmail.com>
2022-01-04 11:20:16 -0600

    created folder structure

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  yelpApiKey : 'ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx',
+  youtubeApiKey: 'AIzaSyCnhGCRCetv8wXTF22ZgL-1m1VcJ4PL5H0'
+}
+
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  yelpApiKey: process.env.YELP_API_KEY,
+  youtubeApiKey:process.env.YOUTUBE_API_KEY
+}
