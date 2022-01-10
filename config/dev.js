050b9543924f97e8d98fcdea0fbc46e9f320bedb
Parvati Mani <parumani@gmail.com>
2022-01-10 08:16:29 -0600

    added MongoURI

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
   yelpApiKey : 'ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx',
-  youtubeApiKey: 'AIzaSyCnhGCRCetv8wXTF22ZgL-1m1VcJ4PL5H0'
+  youtubeApiKey: 'AIzaSyCnhGCRCetv8wXTF22ZgL-1m1VcJ4PL5H0',
+  mongoURI: 'mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority'
 }
 
