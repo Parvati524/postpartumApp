04857d5bb3d3c815e6a3036e0ecec04dd981ce09
Tara Maxwell <taraemaxwell@gmail.com>
2022-01-17 21:27:13 -0600

    Adding UserSchema and Model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,40 @@
-const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose")
+const passportLocalMongoose = require("passport-local-mongoose")
 
-let userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-});
+let userSchema = mongoose.Schema ({
+username: {
+    type: String, 
+    required: [true, "You must enter a username"]
+},
+password: {
+    type: String, 
+    required: true},
+location: {
+    type: String, 
+    required: true
+},
+postpartum_depression: {
+    type: Boolean,
+    required: true},
+postpartum_anxiety: {
+    type: Boolean, 
+    required: true},
+trauma_in_pregnancy: {
+    type: Boolean,
+    required: true},
+trauma_in_birth: {
+    type: Boolean, 
+    required: true},
+physical_pain: {
+    required: true,
+    back: Boolean, 
+    pelvis: Boolean, 
+    abdominal: Boolean
+}
+})
 
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("users", userSchema)
+userSchema.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model("users", userSchema); 
+
+
