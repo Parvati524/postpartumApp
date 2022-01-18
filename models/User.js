cb049854c419f7e82d03e6231073c6d7ba079cc6
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-01-17 21:40:08 -0600

    Taras branch complete

--- models/User.js
+++ models/User.js
@@ -1,40 +1,45 @@
-const mongoose = require("mongoose")
-const passportLocalMongoose = require("passport-local-mongoose")
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-let userSchema = mongoose.Schema ({
-username: {
-    type: String, 
-    required: [true, "You must enter a username"]
-},
-password: {
-    type: String, 
-    required: true},
-location: {
-    type: String, 
-    required: true
-},
-postpartum_depression: {
-    type: Boolean,
-    required: true},
-postpartum_anxiety: {
-    type: Boolean, 
-    required: true},
-trauma_in_pregnancy: {
-    type: Boolean,
-    required: true},
-trauma_in_birth: {
-    type: Boolean, 
-    required: true},
-physical_pain: {
-    required: true,
-    back: Boolean, 
-    pelvis: Boolean, 
-    abdominal: Boolean
-}
+let userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "You must enter a username"]
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    postpartum_depression: {
+        type: Boolean,
+        required: true
+    },
+    postpartum_anxiety: {
+        type: Boolean,
+        required: true
+    },
+    trauma_in_pregnancy: {
+        type: Boolean,
+        required: true
+    },
+    trauma_in_birth: {
+        type: Boolean,
+        required: true
+    },
+    physical_pain: {
+        required: true,
+        back: Boolean,
+        pelvis: Boolean,
+        abdominal: Boolean
+    }
 })
 
 userSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("users", userSchema); 
+module.exports = mongoose.model("users", userSchema);
 
 
