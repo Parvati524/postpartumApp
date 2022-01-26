8a7e4e4c2c462d2a7968f07bf227ac6635488eb7
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-01-26 16:41:59 -0600

    savedVideos feature working

--- public/script.js
+++ public/script.js
@@ -30,3 +30,32 @@ $("#ppdvideowatched").on("click", function () {
     })
     
   });
+
+  $("#ppdvideosaved").on("click", function () {
+        console.log("button works!")
+    let videoId = $("#ppdvideo").attr("src")
+    let username = $(this).attr("class");
+    console.log("username is: " + username)
+    videoId=videoId.split("/")
+    videoId=videoId[videoId.length-1]
+    console.log("video Id is: " + videoId)
+    let endpoint = `http://localhost:3000/${username}/videosSaved`
+    
+    fetch(endpoint, 
+         {method: "PUT",   
+         headers:{'Content-Type':'application/json'},
+         body:JSON.stringify({video:videoId})})
+    .then(function(response){
+      if(!response.ok){
+        throw Error("Issues getting data from server")
+      } else {
+        return response.json()
+      }
+    })
+    .then(function(data){
+      console.log(data)
+    })
+    .catch(function(error){
+      console.error("Error updating: ", error)
+    }) 
+  });
