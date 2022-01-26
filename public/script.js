7a729b8c5894c4f91aff70af783d7ca366922a2d
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-01-26 09:32:03 -0600

    working on button functionality jquery

--- public/script.js
+++ public/script.js
@@ -1,30 +1,31 @@
 //for this code, in line 4 we can give the iframe a particular ID or class Name so we can reference that particular video since we will have several on page. 
 
-// $("button").on("click", function () {
-//     let videoId = $("iframe").attr("src")
-//     let username = this.id;
-//     videoId=videoId.split("/")
-//     videoId=videoId[videoId.length-1]
-//     console.log(videoId)
-//     let endpoint = `http://localhost:3000/${username}/videosWatched`
-//     let self = this;
-//     fetch(endpoint, 
-        //  {method: "PUT",   
-//          headers:{'Content-Type':'application/json'},
-//          body:JSON.stringify({video:videoId})})
-
-//     .then(function(response){
-//       if(!response.ok){
-//         throw Error("Issues getting data from server")
-//       } else {
-//         return response.json()
-//       }
-//     })
-//     .then(function(data){
-//       console.log(data)
-//     })
-//     .catch(function(error){
-//       console.error("Error updating: ", error)
-//     })
+$("#ppdvideobutton").on("click", function () {
+        console.log("button works!")
+    let videoId = $("#ppdvideo").attr("src")
+    let username = this.className;
+    videoId=videoId.split("/")
+    videoId=videoId[videoId.length-1]
+    console.log(videoId)
+    let endpoint = `http://localhost:3000/${username}/videosWatched`
     
-//   });
+    fetch(endpoint, 
+         {method: "PUT",   
+         headers:{'Content-Type':'application/json'},
+         body:JSON.stringify({video:videoId})})
+
+    .then(function(response){
+      if(!response.ok){
+        throw Error("Issues getting data from server")
+      } else {
+        return response.json()
+      }
+    })
+    .then(function(data){
+      console.log(data)
+    })
+    .catch(function(error){
+      console.error("Error updating: ", error)
+    })
+    
+  });
