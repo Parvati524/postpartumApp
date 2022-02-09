cd4857facc08b3213ac4749e648960ed95c7a3a1
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-02-09 10:37:06 -0600

    parenthesis filter method

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.get('/logout', (req, res) => {
 //function to filter one array to only have values that are not included in 2 other arrays.
 function filterArr(arrOne, arrTwo, arrThree) {
     return arrOne.filter(x =>
-        !arrTwo.includes(x.id.videoId) || !arrThree.includes(x.id.videoId)
+        !(arrTwo.includes(x.id.videoId) || arrThree.includes(x.id.videoId))
     )
    
 }
