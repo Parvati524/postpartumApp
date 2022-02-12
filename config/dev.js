14aac77648b4f62de76c055a59cf8f7fc76d5ae1
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-02-12 15:57:06 -0600

    made one video card needs width css

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   yelpApiKey : 'ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx',
-  youtubeApiKey: 'AIzaSyBYlL8qy7SZX-b50CfO6uhkircY_w0AtHA',
+  youtubeApiKey: 'AIzaSyDNrbCUFrJ4sGcYXZjkngXtD4Oe5I9wwlw',
   mongoURIKey: 'mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority'
 }
 
