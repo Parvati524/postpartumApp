fe1f7228079d2f82a060df37054f3625d5ddcb47
ous8292 <ous8292@gmail.com>
2022-02-14 14:31:27 -0600

    added formatting for user page - grid/flexbox

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,7 @@
 module.exports = {
-  yelpApiKey : 'ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx',
-  youtubeApiKey: 'AIzaSyDNrbCUFrJ4sGcYXZjkngXtD4Oe5I9wwlw',
-  mongoURIKey: 'mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority'
-}
-
+  yelpApiKey:
+    "ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx",
+  youtubeApiKey: "AIzaSyCR6zjPspLNgNpfgsIhfy2s6QRtHLrWgWM",
+  mongoURIKey:
+    "mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority",
+};
