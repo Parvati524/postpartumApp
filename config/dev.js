03f693cebbacb480e106e51dc386bce264214d55
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-02-14 14:14:38 -0600

    one card finished

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   yelpApiKey : 'ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx',
-  youtubeApiKey: 'AIzaSyDNrbCUFrJ4sGcYXZjkngXtD4Oe5I9wwlw',
+  youtubeApiKey: 'AIzaSyCM3VuGitKioMkhMv_E4Lzbe-BH3MKS-Ow',
   mongoURIKey: 'mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority'
 }
 
