356ac0abe3d117ba8ba4ca8d315caa23b3aa0084
Parvati <62396105+Parvati524@users.noreply.github.com>
2022-04-18 14:46:05 -0500

    deleted dev.js and deleted it from gitignore in order to hide keys

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  yelpApiKey:
-    "ERVEM9NrMrOGxZYKci2QFbVmTILjHdUjtzhKwovXPJMxo6d0dYSBCpvlN8GggeqhAtFcSiRLVqqOQWh_OBXlHHIWxWusGfLPeeyNjdqZxO7SRyXSTva2gc8qJqSCYXYx",
-  youtubeApiKey: "AIzaSyBqrIor7ejf9HEr2IRg0Mty3LFdBBbWie4",
-  mongoURIKey:
-    "mongodb+srv://Parvati:HZzYELn17vl8YbR3@parvati.pxdkx.mongodb.net/postpartumApp?retryWrites=true&w=majority",
-};
